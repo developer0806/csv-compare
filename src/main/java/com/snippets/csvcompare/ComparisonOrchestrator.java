5b864b67d123a209a402684a8d02b7c6af9b7568
A G <developer0806@gmail.com>
2023-07-02 17:53:38 +0530

    Intermediate commit.

--- src/main/java/com/snippets/csvcompare/ComparisonOrchestrator.java
+++ src/main/java/com/snippets/csvcompare/ComparisonOrchestrator.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -15,11 +20,14 @@ public class ComparisonOrchestrator {
 
 
     public static final String KEY_DELIMITER = ":";
+    public static final String LINE_BREAK = "\n";
     @Value("app.input.old.file")
     private String oldFilename;
 
     @Value("app.input.new.file")
     private String newFilename;
+    @Value("app.output.file")
+    private String outputFileName;
 
     private final FileComparator comparator;
     private final FileToMapConverter fileToMapConverter;
@@ -31,14 +39,24 @@ public class ComparisonOrchestrator {
     }
 
     public void start() {
+        outputFileName+= LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddhhmmss"));
         AtomicLong counter = new AtomicLong(0);
         comparator.compare(
                         fileToMapConverter.convert(oldFilename),
                         fileToMapConverter.convert(newFilename))
                 .filter(RowComparisonResult::hasNoDifference)
                 .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / 1000))
-                .values().parallelStream().map(this::transformResult).collect(Collectors.);
+                .values().stream().map(this::transformResult)
+                .flatMap(strings -> strings.parallelStream().map(String::getBytes))
+                .forEach(this::writeToOutputFile);
+    }
 
+    private void writeToOutputFile(byte[] bytes) {
+        try {
+            Files.write(new File(outputFileName).toPath(),bytes);
+        } catch (IOException e) {
+            throw new IllegalStateException("Error while writing file [" + outputFileName + "]", e);
+        }
     }
 
     private List<String> transformResult(List<RowComparisonResult> rowComparisonResults) {
@@ -48,7 +66,7 @@ public class ComparisonOrchestrator {
                         .filter(CellComparisonResult::isNotDifferent)
                         .map(comparisonResult -> comparisonResult.getKey() + KEY_DELIMITER
                                 + "OldValue: " + comparisonResult.getOldValue()
-                                + " NewValue: " + comparisonResult.getNewValue()))
+                                + " NewValue: " + comparisonResult.getNewValue() + LINE_BREAK))
                 .collect(Collectors.toList());
 
     }
